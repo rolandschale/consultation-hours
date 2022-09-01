2f3098b922c12d6f857c5e42b4bee0443c60748b
Roland Schale <roland.schale@gmail.com>
2022-09-01 20:06:39 +0000

    übersetzungen

--- src/Resources/contao/languages/en/tl_consultation_hours_address.php
+++ src/Resources/contao/languages/en/tl_consultation_hours_address.php
@@ -15,34 +15,40 @@ declare(strict_types=1);
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_consultation_hours']['first_legend'] = "Basis Einstellungen";
-$GLOBALS['TL_LANG']['tl_consultation_hours']['second_legend'] = "Erweiterte Einstellungen";
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['first_legend'] = "Adresse";
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['second_legend'] = "Kontakt";
 
 /**
  * Operations
  */
-$GLOBALS['TL_LANG']['tl_consultation_hours']['edit'] = ["Datensatz mit ID: %s bearbeiten", "Datensatz mit ID: %s bearbeiten"];
-$GLOBALS['TL_LANG']['tl_consultation_hours']['copy'] = ["Datensatz mit ID: %s kopieren", "Datensatz mit ID: %s kopieren"];
-$GLOBALS['TL_LANG']['tl_consultation_hours']['delete'] = ["Datensatz mit ID: %s löschen", "Datensatz mit ID: %s löschen"];
-$GLOBALS['TL_LANG']['tl_consultation_hours']['show'] = ["Datensatz mit ID: %s ansehen", "Datensatz mit ID: %s ansehen"];
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['edit'] = ["Datensatz mit ID: %s bearbeiten", "Datensatz mit ID: %s bearbeiten"];
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['copy'] = ["Datensatz mit ID: %s kopieren", "Datensatz mit ID: %s kopieren"];
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['delete'] = ["Datensatz mit ID: %s löschen", "Datensatz mit ID: %s löschen"];
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['show'] = ["Datensatz mit ID: %s ansehen", "Datensatz mit ID: %s ansehen"];
 
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_consultation_hours']['title'] = ["Titel", "Geben Sie den Titel ein"];
-$GLOBALS['TL_LANG']['tl_consultation_hours']['selectField'] = ["Select Feld", "Wählen Sie aus."];
-$GLOBALS['TL_LANG']['tl_consultation_hours']['checkboxField'] = ["Chosen Feld", "Wählen Sie aus."];
-$GLOBALS['TL_LANG']['tl_consultation_hours']['multitextField'] = ["Multitext Feld", "Geben Sie die Werte ein"];
-$GLOBALS['TL_LANG']['tl_consultation_hours']['addSubpalette'] = ["Erweiterte Einstellungen aktivieren", "Hier können Sie die erweiterten Einstellungen aktivieren."];
-$GLOBALS['TL_LANG']['tl_consultation_hours']['textareaField'] = ["Textarea", "Geben Sie einen Text ein"];
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['name_1'] = ["Name 1", "Geben Sie einen Namen ein"];
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['name_2'] = ["Name 2", "Geben Sie einen Namen ein"];
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['name_3'] = ["Name 3", "Geben Sie einen Namen ein"];
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['street'] = ["Straße", "Geben Sie eine Straße mit Hausnummer ein"];
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['zip'] = ["PLZ", "Geben Sie die PLZ ein"];
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['city'] = ["Ort", "Geben Sie den Ortsnamen ein"];
+
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['phone'] = ["Telefon", "Geben Sie eine Telefonnummer ein"];
+// $GLOBALS['TL_LANG']['tl_consultation_hours_address'][''] = ["Chosen Feld", "Wählen Sie aus."];
+// $GLOBALS['TL_LANG']['tl_consultation_hours_address'][''] = ["Multitext Feld", "Geben Sie die Werte ein"];
+// $GLOBALS['TL_LANG']['tl_consultation_hours_address']['addSubpalette'] = ["Erweiterte Einstellungen aktivieren", "Hier können Sie die erweiterten Einstellungen aktivieren."];
+// $GLOBALS['TL_LANG']['tl_consultation_hours_address'][''] = ["", "Geben Sie einen Text ein"];
 
 /**
  * References
  */
-$GLOBALS['TL_LANG']['tl_consultation_hours']['firstoption'] = "Erste Option";
-$GLOBALS['TL_LANG']['tl_consultation_hours']['secondoption'] = "Zweite Option";
+// $GLOBALS['TL_LANG']['tl_consultation_hours_address']['firstoption'] = "Erste Option";
+// $GLOBALS['TL_LANG']['tl_consultation_hours_address']['secondoption'] = "Zweite Option";
 
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_consultation_hours']['customButton'] = "Custom Routine starten";
+$GLOBALS['TL_LANG']['tl_consultation_hours_address']['customButton'] = "Custom Routine starten";
